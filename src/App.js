a02cdf640e9f5451b65b5893852baba358aa2bd0
Mohsin Raza <mohsin.tech101@gmail.com>
2023-03-07 20:46:29 +0530

    News are coming to the news Component and thereby shown in the NewsItem component successfully, Next task to fetch news dynamically by using the newsAPI end-points either in using axios or fetch method

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
 
-  c = "Mohsin Raza"
 
   render() {
     return (
+      
       <div>
-          Hello {this.c}!
+        <NavBar />
+        <News />
       </div>
     )
   }
